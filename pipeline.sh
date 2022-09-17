594a3703208016ca5bbd617c89c05f798ab696cb
patrickbelding <patrick.m.belding@gmail.com>
2022-09-17 01:03:03 -0400

    commit

--- pipeline.sh
+++ pipeline.sh
@@ -3,6 +3,6 @@
 python3 /home/pi/current_reservations.py 
 cd /home/pi/github_pipeline/
 /usr/bin/git init
-/usr/bin/git add -A
+/usr/bin/git add .
 /usr/bin/git commit -am "commit"
 /usr/bin/git push -u origin master
