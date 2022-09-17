88765f74047bd61657e4c60510366e645fbe58d6
patrickbelding <patrick.m.belding@gmail.com>
2022-09-17 01:16:03 -0400

    update

--- pipeline.sh
+++ pipeline.sh
@@ -5,6 +5,5 @@ cd /home/pi/github_pipeline/
 /usr/bin/git init
 /usr/bin/git add .
 /usr/bin/git commit -am "update"
-# This action will pull and push commits from and to 'origin/main'.
 /usr/bin/git pull origin main
 /usr/bin/git push -u origin main
