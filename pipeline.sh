6cb9446ef32b9a01edd05b83cea13637f165030a
patrickbelding <patrick.m.belding@gmail.com>
2022-09-17 01:06:02 -0400

    commit

--- pipeline.sh
+++ pipeline.sh
@@ -5,5 +5,7 @@ cd /home/pi/github_pipeline/
 /usr/bin/git init
 /usr/bin/git add .
 /usr/bin/git commit -am "commit"
+/usr/bin/git pull
 /usr/bin/git push -u origin master
+/usr/bin/git pull
 /usr/bin/git fetch --all
