92f405ea8db84d3498a9973b0babd3c11f065ea1
patrickbelding <patrick.m.belding@gmail.com>
2022-09-17 00:28:03 -0400

    commit at 20220917002803

--- pipeline.sh
+++ pipeline.sh
@@ -2,8 +2,6 @@
 
 python3 /home/pi/current_reservations.py 
 
-REPO_DIR=/home/pi/github_pipeline
-cd ${REPO_DIR}
 git -C /home/pi/github_pipeline init
 git -C /home/pi/github_pipeline add .
 TIME=`date +%Y%m%d%H%M%S`
