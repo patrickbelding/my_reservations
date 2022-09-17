adfd4a3a007e34462c8116ce811ab3ca18830fc1
patrickbelding <patrick.m.belding@gmail.com>
2022-09-17 01:15:03 -0400

    update

--- pipeline.sh
+++ pipeline.sh
@@ -4,9 +4,7 @@ python3 /home/pi/current_reservations.py
 cd /home/pi/github_pipeline/
 /usr/bin/git init
 /usr/bin/git add .
-# get the current date and time
-now=$(date +"%m_%d_%Y")
-/usr/bin/git commit -am "update $now"
+/usr/bin/git commit -am "update"
 # This action will pull and push commits from and to 'origin/main'.
 /usr/bin/git pull origin main
 /usr/bin/git push -u origin main
