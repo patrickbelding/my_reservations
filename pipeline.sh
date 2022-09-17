91bf0940db4543b2a15a968a17bbd21b7d4ce816
patrickbelding <patrick.m.belding@gmail.com>
2022-09-17 00:41:36 -0400

    commit

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 python3 /home/pi/current_reservations.py 
+cd /home/pi/github_pipeline
 git init
 git add .
 TIME=`date +%Y%m%d%H%M%S`
 git commit -m "commit"
-git push -u origin master
+git push 
