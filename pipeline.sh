f23c016bf757697df33555b6fe96ae559d962294
patrickbelding <patrick.m.belding@gmail.com>
2022-09-16 23:40:47 -0400

    commit at 20220916234047

--- pipeline.sh
+++ pipeline.sh
@@ -5,5 +5,7 @@ REPO_DIR=/home/pi/github_pipeline
 cd ${REPO_DIR}
 git init
 git add .
-git commit -m 'Updating List'
+# get current time
+TIME=`date +%Y%m%d%H%M%S`
+git commit -m "commit at ${TIME}"
 git push -u origin master
